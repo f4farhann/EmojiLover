535a40aba7daea1eeb06d784bfc7a52f078ad0c3
farhan haider <f4farhann@gmail.com>
2024-08-19 15:37:03 +0530

    Initial Commit

---
+++ EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by farhan haider on 19/08/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
