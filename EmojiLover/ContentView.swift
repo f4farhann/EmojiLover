0a1f65dc4904ae3327a1248e35d4fc1c8115a9ba
farhan haider <f4farhann@gmail.com>
2024-08-23 23:42:48 +0530

    animation added to emoji

--- EmojiLover/ContentView.swift
+++ EmojiLover/ContentView.swift
@@ -13,11 +13,24 @@ enum Emoji: String, CaseIterable {
 struct ContentView: View {
     
     @State var selection: Emoji = .😀
+        @State private var scale: CGFloat = 1.0
+
+    
     var body: some View {
         NavigationView{
             VStack {
                 Text(selection.rawValue)
                     .font(.system(size: 150))
+                    .scaleEffect(scale)
+                    .animation(.easeInOut(duration: 0.5), value: scale)
+                    .onChange(of: selection) {
+                        withAnimation {
+                            scale = 1.2
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) {
+                                scale = 1.0
+                            }
+                        }
+                    }
                 
                 Picker("Select Emoji", selection: $selection){
                     ForEach(Emoji.allCases, id: \.self){ Emoji in
